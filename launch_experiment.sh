42571ba9414f08297e730767af177bfc6da81616
youssefp <paul.youssef@univ-grenoble-alpes.fr>
2023-01-04 23:28:05 +0100

    cleaning

--- launch_experiment.sh
+++ launch_experiment.sh
@@ -8,38 +8,6 @@ if [[ $1 == "help" ]];then
     exit 1
 fi
 
-if [[ $1 == "-t" ]];then
-    rm .*.log 2> /dev/null
-    for((i=0;i<$num_nodes;i++));  
-    do
-        name="node"${i}
-        node_name="node_"${i}
-        node=$(get_config NODEINFO $node_name $node_list_path)
-        node_config_param="tmp/param_${node_name}.conf"
-        echo "Retrieving parameters from node"
-        #echo "ssh $user@$server walt node cp $node:/persist/param.conf $node_config_param" 
-        ssh $user@$server walt node cp $node:/persist/param.conf $node_config_param
-        scp $user@$server:$node_config_param $node_config_param
-        diff -q -b -B $local_config_param $node_config_param > /dev/null
-        echo $local_config_param $node_config_param
-        if [[ $? != 0 ]]; then
-            echo "Error ! Local node parameter different from remote node parameter. Please send param.conf"
-            exit 1
-        fi
-        local_node_graph_conf="./graphs/$node_name.conf"
-        remote_node_graph_conf="tmp/graph_${node_name}.conf"
-        echo "Retrieving remote node graphs conf from node"
-        echo "ssh $user@$server walt node cp $node:/persist/my_id.conf $remote_node_graph_conf"
-        ssh $user@$server walt node cp $node:/persist/my_id.conf $remote_node_graph_conf
-        scp $user@$server:$remote_node_graph_conf $remote_node_graph_conf
-        diff -q -b -B $local_node_graph_conf $remote_node_graph_conf > /dev/null
-        if [ $? != 0 ]; then
-            echo "Error ! Local node graph different from remote node graph. Please send graphs"
-            exit 1
-        fi
-    done
-fi
-
 
 echo "Running experiment"
 
